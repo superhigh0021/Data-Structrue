c3d1767a1d30efcdb55c28e56582478909ed3ed2
superhigh0021 <975440488@qq.com>
2021-03-30 13:28:18 +0800

    1

---
+++ HFUT Data Structure/Unit 5/1.cpp
@@ -0,0 +1,39 @@
+#include "List.h"
+#include <iostream>
+using namespace std;
+
+template <typename T>
+void Union(List<T>& l1, List<T>& l2)
+{
+    cout<<"交集：";
+    cout << "{";
+    for (int i = 0; i < l1.size(); ++i) {
+        if (l2.locate(l1.get_element(i)) != -1)
+            cout << l1.get_element(i) << ' ';
+    }
+    cout << "}" << endl;
+}
+
+template <typename T>
+void Intersection(List<T>& l1, List<T>& l2)
+{
+    cout<<"并集：";
+    cout << "{";
+    for (int i = 0; i < l1.size(); ++i) {
+        if (l2.locate(l1.get_element(i)) != -1)
+            l1.Delete(i);
+    }
+    l1.PrintTest();l2.PrintTest();
+    cout<<"}"<<endl;
+}
+
+int main(void)
+{
+    int a[] = { 1, 3, 5, 7 };
+    int b[] = { 2, 5, 8, 6 };
+    List<int> l1(a, 4), l2(b, 4);
+    Union(l1, l2);
+    Intersection(l1,l2);
+    system("pause");
+    return 0;
+}
--- HFUT Data Structure/Unit 5/List.h
+++ HFUT Data Structure/Unit 5/List.h
@@ -1,63 +1,85 @@
 #include <iostream>
 using namespace std;
-#define maxlength 10
+#define maxlength 20
 
 template <typename T>
-class List
-{
+class List {
 public:
     List();
+    List(T a[], int n);
     int size()
     {
         return count;
     }
-    T get_element(const int &i) const;
-    int locate(const T &x);
-    void insert(const int &i, const T &x);
-    void Delete(const int &i);
+    T get_element(const int& i) const;
+    int locate(const T& x);
+    void insert(const int& i, const T& x);
+    void Delete(const int& i);
+    void PrintTest();
 
 private:
-    int count;
+    int count=0;
     T data[maxlength];
 };
 
 template <typename T>
-List<T>::List()
+List<T>::List(T a[], int n)
 {
-    count = 0;
-}
-
-template <typename T>
-T List<T>::get_element(const int &i) const
-{
-    if (i <= 0 || i > count)
+    if (n > maxlength) {
+        cout << "overflow!" << endl;
         exit(0);
-    return data[i - 1];
+    }
+    for (int i = 0; i < n; ++i) {
+        data[i] = a[i];
+        count++;
+    }
 }
 
 template <typename T>
-int List<T>::locate(const T &x)
+T List<T>::get_element(const int& i) const
+{
+    if (i < 0 || i >= count)
+        exit(0);
+    return data[i];
+}
+
+template <typename T>
+int List<T>::locate(const T& x)
 {
     for (int i = 0; i < size(); ++i)
         if (data[i] == x)
-            return (i + 1);
-    return 0;
+            return (i);
+    return -1;
 }
 
 template <typename T>
-void List<T>::insert(const int &i, const T &x)
+void List<T>::insert(const int& i, const T& x)
 {
     if (count == maxlength)
         exit(0);
-    if (i < 1 || i > size() + 1)
+    if (i < 0 || i >= size())
         exit(0);
-    for (int j = count; j >= i; --j)
-        data[j] = data[j - 1];
-    data[i - 1] = x;
     count++;
+    for (int j = count-1; j > i; --j)
+        data[j] = data[j - 1];
+    data[i] = x;
 }
 
-template<typename T>
-void List<T>::Delete(const int& i){
-    if(size()==0) return;
+template <typename T>
+void List<T>::Delete(const int& i)
+{
+    if (size() == 0)
+        exit(0);
+    if (i < 1 || i > size())
+        exit(0);
+    for (int j = i + 2; j < count+1; ++j)
+        data[j - 2] = data[j - 1];
+    count--;
+}
+
+template <typename T>
+void List<T>::PrintTest()
+{
+    for (int i = 0; i < count; ++i)
+        cout << data[i] << ' ';
 }
