56c39fd0f8e17dccd1a82011c623abab61c8da48
superhigh0021 <975440488@qq.com>
2021-06-23 17:19:14 +0800

    1

--- Algorithm_4th/Unit2 Sorting/Sorting_test.cpp
+++ Algorithm_4th/Unit2 Sorting/Sorting_test.cpp
@@ -33,8 +33,8 @@ void heap_test()
 int main(void)
 {
     //Merge_test();
-    //Quick_test();
-    heap_test();
+    Quick_test();
+    //heap_test();
 
     system("pause");
     return 0;
--- test01.cpp
+++ test01.cpp
@@ -1,30 +1,61 @@
 #include <iostream>
 using namespace std;
+#include <vector>
 
-struct node {
-    node* next = nullptr;
-    int data;
-};
-
-void change(node* p)
+void percolate_down(vector<int>& v, int i, int n)
 {
-    while (p->next != nullptr)
-        p = p->next;
+    while (i * 2 <= n - 1) {
+        int j = i * 2 + 1;
+        if (j < n && v[j] < v[j + 1])
+            ++j;
+        if (v[i] < v[j])
+            swap(v[i], v[j]);
+        i = j;
+    }
+}
+
+void heap_sort(vector<int>& v, int k)
+{
+    int n = v.size();
+    for (int i = n / 2 - 1; i >= 0; --i) {
+        percolate_down(v, i, n);
+    }
+    --n;
+    int m = k;
+    while (n > 0 && k > 0) {
+        swap(v[0], v[n--]);
+        percolate_down(v, 0, n);
+        --k;
+    }
+    cout << v[v.size() - m];
+}
+
+void adjustHeap(int* A, int idx, int n)
+{
+    int ldx = 2 * idx + 1;
+    int rdx = 2 * idx + 2;
+    int maxdx = idx;
+
+    if (idx <= n / 2) {
+        if (ldx < n && A[ldx] > A[maxdx]) {
+            maxdx = ldx;
+        }
+
+        if (rdx < n && A[rdx] > A[maxdx]) {
+            maxdx = rdx;
+        }
+
+        if (maxdx != idx) {
+            swap(A[idx], A[maxdx]);
+            adjustHeap(A, maxdx, n);
+        }
+    }
 }
 
 int main(void)
 {
-    node* head = new node;
-    head->data = 1;
-    head->next = new node;
-    head->next->data = 2;
-    head->next->next = new node;
-    head->next->next = new node;
-    head->next->next->data = 3;
-    node* p = head;
-
-    change(p);
-    cout << p->data;
+    vector<int> v = { 7, 3, 2, 0, 1, 8, 5, 6, 4 };
+    heap_sort(v, 3);
 
     system("pause");
 
