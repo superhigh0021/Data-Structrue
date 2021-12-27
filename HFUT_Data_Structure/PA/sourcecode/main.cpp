d80b0d08be9cf2bd7581d915891685cdbdc77975
superhigh0021 <975440488@qq.com>
2021-12-27 22:51:34 +0800

    get_back_my_data

---
+++ HFUT_Data_Structure/PA/sourcecode/main.cpp
@@ -0,0 +1,45 @@
+/*设计散列表实现电话号码查找系统。 
+ 
+1) 设每个记录有下列数据项：用户名、电话号码、地址； 
+2) 从键盘输入各记录，以用户名（汉语拼音形式）为关键字建立散列表；
+3) 采用一定的方法解决冲突； 
+4) 查找并显示给定电话号码的记录；  */
+
+#include "TelephoneNumber.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(void)
+{
+    TelephoneNumber tn;
+    char choice = 0;
+    while (true) {
+        tn.showmenu();
+        cout << "请输入你的选择：";
+        cin >> choice;
+        switch (choice) {
+        case '0':
+            tn.exitsystem();
+            break;
+        case '1':
+            cin.clear();
+            tn.push();
+            break;
+        case '2':
+            cin.clear();
+            tn.find();
+            break;
+        default:
+            cout << "输入格式有误！请重新输入。" << endl;
+            cin.clear();
+            cin.sync();
+            system("pause");
+            system("cls");
+            continue;
+        }
+    }
+
+    system("pause");
+    return 0;
+}
--- test01.cpp
+++ test01.cpp
@@ -1,16 +1,12 @@
-#include <iostream>
-#include <cstring>
+#include<iostream>
 using namespace std;
 
+template<int T>
+void print(){
+    cout<<2*T<<endl;
+}
 
-int main(void)
-{
-    char s[] = "abcdefg";
-    char *t = s + strlen(s);
-    if ("a" > t)
-        cout << "yes!" << endl;
-    else
-        cout << "no!" << endl;
-
+int main(int argc,char**argv){
+    print<2>();
     return 0;
 }
